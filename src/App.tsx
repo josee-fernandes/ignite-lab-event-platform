c99f9cd0a912a713312ad08290075644548a49d2
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2022-06-22 23:57:11 -0300

    Setup graphqq, apollo for api requests & graphcms with database

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface ILesson {
+  id: string
+  title: string
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: ILesson[]}>(GET_LESSONS_QUERY)
+
+  console.log(data)
+
+  // useEffect(() => {
+  //   client.query({
+  //     query: GET_LESSONS_QUERY
+  //   }).then(console.log)
+  // }, [])
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {data?.lessons.map(lesson => <li key={lesson.id}>{lesson.title}</li>)}
+    </ul>
   )
 }
 
